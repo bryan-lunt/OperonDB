e19b27697437b822d5af44e101976f60b423384e
Bryan Lunt <bryan.j.lunt@gmail.com>
2016-08-14 20:05:43 +0000

    altered script

--- scripts/get_uniprot.bash
+++ scripts/get_uniprot.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+basedir=$1
+mkdir -p ${basedir}
+
+pushd ${basedir}
+
 curl ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/README > README.uniprot
 curl ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/bacteria/assembly_summary.txt > assembly_summary_bacteria.txt
 curl ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/archaea/assembly_summary.txt > assembly_summary_archaea.txt
@@ -12,4 +17,4 @@ mkdir -p refproteome_id_mappings
 #(cd refproteome_id_mappings; wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Bacteria/*idmapping.gz)
 #(cd refproteome_id_mappings; wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Archaea/*idmapping.gz)
 
-
+popd
