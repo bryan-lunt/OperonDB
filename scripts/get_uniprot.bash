aeaa319d21940ddebeaf4be72dfa7cca53f8902e
Bryan Lunt <bryan.j.lunt@gmail.com>
2016-08-14 21:27:52 +0000

    updated script

--- scripts/get_uniprot.bash
+++ scripts/get_uniprot.bash
@@ -11,7 +11,16 @@ curl ftp://ftp.ncbi.nlm.nih.gov/genomes/genbank/archaea/assembly_summary.txt > a
 
 
 #get the IDs of all the proteomes so we can look up which genome assemblies they come from.
+#unfortunately, this doed not limit itself to bacteria and archaea....
 awk '/Proteome_ID/,/^$/ {print $0;}' README.uniprot | head -n-1 | tail -n+2 > uniprot_proteome_list.txt
+
+#get the lists of available Bacteria and Archae proteomes.
+curl ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Bacteria/ > uniprot_Bacteria_filelist.txt
+curl ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Archaea/ > uniprot_Archaea_filelist.txt
+
+#get just a list of uniprot ids and (taxids?)
+awk '/ UP/' uniprot_Archaea_filelist.txt | sed 's/^.*UP\([[:digit:]]\+\)_\([[:digit:]]\+\).*/UP\1	\2/;' | uniq > uniprot_Archaea_ids.txt
+awk '/ UP/' uniprot_Bacteria_filelist.txt | sed 's/^.*UP\([[:digit:]]\+\)_\([[:digit:]]\+\).*/UP\1 \2/;' | uniq > uniprot_Bacteria_ids.txt
 
 mkdir -p refproteome_id_mappings
 #(cd refproteome_id_mappings; wget ftp://ftp.uniprot.org/pub/databases/uniprot/current_release/knowledgebase/reference_proteomes/Bacteria/*idmapping.gz)
